609cad098f4fef828e0df1045e5ba74ff601c308
edzonee <edzone1996@gmail.com>
2023-02-16 08:46:07 +0100

    Updates

--- src/Components/Accordion/ExpandebleColumn.jsx
+++
@@ -1,9 +0,0 @@
-export const ExpandebleColumn = ({ description }) => {
-  return (
-    <div className="column-container">
-      <div className="column-text">{description}</div>
-    </div>
-  );
-};
-
-export default ExpandebleColumn;
--- src/Components/Accordion/TextSection.jsx
+++
@@ -1,6 +0,0 @@
-export const TextSection = () => {
-    return <div className="column-container">text section</div>;
-  };
-  
-  export default TextSection;
-  
